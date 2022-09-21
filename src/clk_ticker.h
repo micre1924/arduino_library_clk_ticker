63eabdc5491192e895c49c0c65a8fc8335489159
Michael Reul <77065376+micre1924@users.noreply.github.com>
2022-09-21 15:07:24 +0200

    Update clk_ticker.h

--- src/clk_ticker.h
+++ src/clk_ticker.h
@@ -1,32 +1,18 @@
-/*
- Name:		smy_clk_ticker.h
- Created:	5/20/2021 2:21:30 PM
- Author:	GPD
- Editor:	http://www.visualmicro.com
-*/
+#include "Arduino.h"
 
-#ifndef _smy_clk_ticker_h
-#define _smy_clk_ticker_h
+namespace mrc{
+	class clkTicker {
+	public:
+		unsigned long OriginTimePoint = 0, PeriodeInTime = 0, Period = 0, lastTickTimestamp = 0;
+		bool isMicro = false;
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "Arduino.h"
-#else
-	#include "WProgram.h"
-#endif
+		clkTicker(unsigned long periode, bool is_micro = false);
 
-class clkTicker {
-public:
-	unsigned long OriginTimePoint = 0, PeriodeInTime = 0, Period = 0, lastTickTimestamp = 0;
-	bool isMicro = false;
+		clkTicker();
 
-	clkTicker(unsigned long periode, bool is_micro = false);
+		bool tick();
 
-	clkTicker();
-
-	bool tick();
-
-	bool reset();
-};
-
-#endif
+		void reset();
+	};
+}
 
